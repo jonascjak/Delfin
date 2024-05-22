e826ee27053ab24f85048dc786958c173f0a8218
jonascjak <jonascjak@gmail.com>
2024-05-22 04:12:23 +0200

    Added ScannerHandler

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Members.CompetitionMember;
 import Members.Member;
 import Utility.Filehandler;
 
@@ -6,6 +7,9 @@ import java.util.ArrayList;
 
 public class Main implements Serializable {
     public static ArrayList<Member> members = Filehandler.readMembersFromFile();
+    public static ArrayList<Member> juniorTeam = Filehandler.readMembersFromFile();
+    public static ArrayList<Member> seniorTeam = Filehandler.readMembersFromFile();
+
     public static void main(String[] args) {
 
     }
