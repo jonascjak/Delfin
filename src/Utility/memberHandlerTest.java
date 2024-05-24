e78548023e321111e8acefb0aa5ab15ad653afb8
elisa236164 <s236164@dtu.dk>
2024-05-24 12:19:24 +0200

    unit test korrekt

--- src/Utility/memberHandlerTest.java
+++ src/Utility/memberHandlerTest.java
@@ -1,27 +1,33 @@
 package Utility;
-
+import Members.Member;
 import static org.junit.Assert.assertEquals;
-
-import Utility.memberHandler;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class memberHandlerTest {
 
-    private memberHandler handler;
-
     @Before
     public void setUp() {
-        handler = new memberHandler();
+        memberHandler.memberList = new ArrayList<>();
     }
 
     @Test
-    public void testMinimumKontingent() {
-        double expectedMinKontingent = 100.0;
+    public void testRevenue() {
+        Member member1 = new Member(1,"John Doe", LocalDate.of(2006, 4, 1), true, false);
+        Member member2 = new Member(2,"Emma Pedersen", LocalDate.of(1955, 5, 1), false, true);
+        Member member3 = new Member(3,"James Smith", LocalDate.of(2000, 6, 23), false, true);
 
-        double actualMinKontingent = handler.calculateMinimumKontingent();
+        memberHandler.memberList.add(member1);
+        memberHandler.memberList.add(member2);
+        memberHandler.memberList.add(member3);
 
-        assertEquals(expectedMinKontingent, actualMinKontingent, 0.01);
+        int expectedRevenue = 1500;
+
+        int actualRevenue = memberHandler.revenue();
+
+        assertEquals(expectedRevenue, actualRevenue);
     }
 }
